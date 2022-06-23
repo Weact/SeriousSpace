fd0b0274eefbfcda3a073417b5c4393a01ff906d
Weact <lucas.druckes@numericable.fr>
2022-06-23 12:43:44 +0200

    web version

--- SCENES/Level/BackgroundScroller.gd
+++
@@ -1,4 +0,0 @@
-extends ParallaxBackground
-const scrolling_speed : float = 5.0
-func _physics_process(delta: float) -> void:
-	scroll_offset += Vector2(scrolling_speed * delta, scrolling_speed * delta)
